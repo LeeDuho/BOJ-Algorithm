bc2e3c5173b56ed10823d32e0e1a3d82af154260
LeeDuho <80142640+LeeDuho@users.noreply.github.com>
2023-01-01 20:14:52 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num <0 || num >100)
+        return 0;
+    if(num>=90 && num <=100)
+        cout << "A";
+    else if(num>=80 && num <=89)
+        cout << "B";
+    else if(num>=70 && num <=79)
+        cout << "C";
+    else if(num>=60 && num <=69)
+        cout << "D";
+    else
+        cout << "F";
+    return 0;
+}
