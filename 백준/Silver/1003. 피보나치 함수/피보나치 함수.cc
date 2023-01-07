c0484c4c41b435eea5535b4ba6abc1c7c09bbd32
LeeDuho <80142640+LeeDuho@users.noreply.github.com>
2023-01-07 20:59:14 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -3,42 +3,18 @@
 
 using namespace std;
 
-vector<pair<int, int>> v(41);
+// vector<pair<int, int>> v(41);
 
-pair<int, int> fibonacci(int n) {
-  if (n == 0) {
-    return { 1, 0 };
-  } else if (n == 1) {
-    return { 0, 1 };
-  } else {
-    if (v[n].first == 0 && v[n].second == 0) {
-      v[n] = { fibonacci(n - 1).first + fibonacci(n - 2).first, fibonacci(n - 1).second + fibonacci(n - 2).second };
-    }
-    return v[n];
-  }
-}
-
-int main() {
-  int T, n;
-  cin >> T;
-
-  for (int i = 0; i < T; i++) {
-    cin >> n;
-    pair<int, int> p = fibonacci(n);
-    cout << p.first << " " << p.second << "\n";
-  }
-
-  return 0;
-}
-
-
-// vector<int> zero(41);
-// vector<int> one(41);
-
-// void fibonacci(int n) {
-//   for (int i = 2; i <= n; i++) {
-//     zero[i] = zero[i - 1] + zero[i - 2];
-//     one[i] = one[i - 1] + one[i - 2];
+// pair<int, int> fibonacci(int n) {
+//   if (n == 0) {
+//     return { 1, 0 };
+//   } else if (n == 1) {
+//     return { 0, 1 };
+//   } else {
+//     if (v[n].first == 0 && v[n].second == 0) {
+//       v[n] = { fibonacci(n - 1).first + fibonacci(n - 2).first, fibonacci(n - 1).second + fibonacci(n - 2).second };
+//     }
+//     return v[n];
 //   }
 // }
 
@@ -46,18 +22,41 @@ int main() {
 //   int T, n;
 //   cin >> T;
 
-//   zero[0] = 1;
-//   zero[1] = 0;
-//   one[0] = 0;
-//   one[1] = 1;
-
-//   fibonacci(40);
-
-//   //입력받은 수만큼 0, 1 호출 횟수 출력
 //   for (int i = 0; i < T; i++) {
 //     cin >> n;
-//     cout << zero[n] << " " << one[n] << endl;
+//     pair<int, int> p = fibonacci(n);
+//     cout << p.first << " " << p.second << "\n";
 //   }
 
 //   return 0;
 // }
+
+
+vector<int> zero(41);
+vector<int> one(41);
+
+void fibonacci(int n) {
+  for (int i = 2; i <= n; i++) {
+    zero[i] = zero[i - 1] + zero[i - 2];
+    one[i] = one[i - 1] + one[i - 2];
+  }
+}
+
+int main() {
+  int T, n;
+  cin >> T;
+
+  zero[0] = 1;
+  zero[1] = 0;
+  one[0] = 0;
+  one[1] = 1;
+
+  fibonacci(40);
+
+  for (int i = 0; i < T; i++) {
+    cin >> n;
+    cout << zero[n] << " " << one[n] << endl;
+  }
+
+  return 0;
+}
