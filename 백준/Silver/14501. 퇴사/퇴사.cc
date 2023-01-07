495e064a9b46577e278cdfd9ee4d5ef835a6ec39
LeeDuho <80142640+LeeDuho@users.noreply.github.com>
2023-01-07 16:57:52 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int N;
+int T[16];
+int P[16];
+int maxProfit = 0;
+
+void dfs(int day, int profit){
+  if(day == N + 1){
+    if(profit > maxProfit){
+      maxProfit = profit;
+    }
+    return;
+  }
+  if(day > N + 1){
+    return;
+  }
+  dfs(day + 1, profit);
+  dfs(day + T[day], profit + P[day]);
+}
+
+int main(){
+  cin >> N;
+  for(int i = 1; i <= N; i++){
+    cin >> T[i] >> P[i];
+  }
+  dfs(1, 0);
+  cout << maxProfit << endl;
+
+  return 0;
+}
