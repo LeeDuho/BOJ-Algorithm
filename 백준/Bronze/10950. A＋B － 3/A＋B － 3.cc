528e62d5aca24f9d8bb90e218898bafbec5ed6d3
LeeDuho <80142640+LeeDuho@users.noreply.github.com>
2023-01-10 23:56:58 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int T;
+
+
+int main(){
+  cin >> T;
+  for(int i=0; i<T;i++){
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << "\n";
+  }
+  return 0;
+}
