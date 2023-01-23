e0f21fced50a19639903f32ce6fb72d338ff9b17
LeeDuho <80142640+LeeDuho@users.noreply.github.com>
2023-01-23 21:09:00 +0900

    [Silver I] Title: 뒤풀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14575. 뒤풀이/뒤풀이.cc
+++ 백준/Silver/14575. 뒤풀이/뒤풀이.cc
@@ -4,41 +4,50 @@
 
 using namespace std;
 
-int n,t,lsum,dl[1001],dr[1001];
+int N, T;
+vector <pair<int,int>> person;
 
-int main () {
-  cin >> n >> t;
-
-	for(int i=0;i<n;i++){
-    cin >> dl[i] >> dr[i];
-    lsum += dl[i];
-	}
-
-  sort(dl,dl+n);
-	sort(dr,dr+n);
-
-	int mid,ret=-1,left=1,right=1000000,idxl,idxr,tsum;
-
-	while(left<=right){
-		mid = (left+right)/2;
-
-		idxl = upper_bound(dl,dl+n,mid)-dl; 
-		idxr = lower_bound(dr,dr+n,mid)-dr;
-		tsum = mid * n;
-
-		for(int i=0;i<idxr;i++) tsum -= mid-dr[i];
-
-		if(idxl<n || tsum<t) left = mid+1;
-		else if(lsum>t) right = mid-1;
-		else{
-			right = mid-1;
-			ret = mid;
-		}
-	}
-
-	printf("%d\n",ret);
-
-	return 0;
-
+int binary_search(int low, int high){
+  int mid;
+  while (low < high){
+    int sum = 0;
+    mid = (low + high) / 2;
+    for (int i = 0; i < person.size(); i++) {
+      if (person[i].first > mid) {
+        sum = 0;
+        break;
+      }
+      sum += min(mid, person[i].second);
+    }
+    if(sum < T){
+      low = mid + 1;
+    }
+    else{
+      high = mid;
+    }
+  }
+  return high;
 }
 
+int main()
+{
+  cin >> N >> T;
+
+  int min = 0, max = 0;
+
+  for(int i = 0; i < N ; i++){
+    int L, R;
+    cin >> L >> R;
+    person.push_back({L,R});
+    min += L;
+    max += R;
+  }
+  if(min > T || max < T){
+    cout << -1 << endl;
+  }
+  else{   
+    cout << binary_search(0,1000000000) << "\n";
+  }
+
+  return 0;
+}
