bb689a83150a0212581c262ed7adede10a4fbe2b
LeeDuho <80142640+LeeDuho@users.noreply.github.com>
2023-01-26 23:44:18 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int hansu(int n){
+  int count = 0;
+  for(int i=1; i<=n; i++){
+    if(i < 100){
+      count++;
+    }
+    else{
+      int a = i / 100;
+      int b = (i % 100) / 10;
+      int c = i % 10;
+      if((a - b) == (b - c)){
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+int main(){
+  int n;
+  cin >> n;
+  cout << hansu(n);
+
+  return 0;
+}
