37c295c4e702c660b3fc2f7890dd8b36196290ff
Lee Duho <80142640+LeeDuho@users.noreply.github.com>
2024-03-24 18:33:56 +0900

    [Gold II] Title: 트리의 지름, Time: 812 ms, Memory: 74428 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -6,15 +6,16 @@ v = int(input())
 
 tree = [[] for _ in range(v+1)]
 
-# for i in range(1,v+1):
-#   temp = list(map(int,input().split()))
-#   for j in range(1, len(temp)-1,2):
-#     tree[i].append([temp[j],temp[j+1]])
+for i in range(1,v+1):
+  temp = list(map(int,input().split()))
+  for j in range(1, len(temp)-1,2):
+    tree[temp[0]].append([temp[j],temp[j+1]])
 
-for _ in range(v):
-    node = list(map(int, input().split()))[:-1]
-    for i in range(1, len(node)//2 + 1):
-        tree[node[0]].append([node[i*2 - 1], node[i*2]])
+# for _ in range(v):
+#     node = list(map(int, input().split()))[:-1]
+#     for i in range(1, len(node)//2 + 1):
+#         tree[node[0]].append([node[i*2 - 1], node[i*2]])
+
 
 def dfs(i,total_len):
 
